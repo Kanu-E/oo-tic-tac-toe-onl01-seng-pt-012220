6909ffbf480d6566272da89df545a8457d6e4216
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-16 20:33:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,14 @@ class TicTacToe
    def won? 
      winner = nil
      WIN_COMBINATIONS.each do |combo|
-       
+       if combo.all? {|win| @board[win == "X"]
+         winner = combo
+       elsif combo.all? {|win| @board[win == "O"]
+         winner = combo
+       else
+         winner
+       end
+       winner
    end
 end
 
